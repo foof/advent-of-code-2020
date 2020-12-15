0eb316f630f6346f5b28b3932b49b535511d1c4a
Jonas Holmström <jonas.holmstrom@consector.se>
2020-12-15 11:38:02 +0100

    Cleanup

--- 15/1.py
+++ 15/1.py
@@ -11,13 +11,8 @@ for t, starting_number in enumerate(data):
 # Start game
 for t in range(len(data), 2020):
     if prev in mem:
-        last_t = mem[prev]
-
-        if last_t == t - 1:
-            speak = 0
-        else:
-            speak = (t - 1) - mem[prev]
-            mem[prev] = t - 1
+        speak = (t - 1) - mem[prev]
+        mem[prev] = t - 1
     else:
         speak = 0
         mem[prev] = t - 1

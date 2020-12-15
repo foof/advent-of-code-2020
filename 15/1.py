a7c87f374837c7fe22efc8720f7e8f1e9e9733c5
Jonas Holmström <jonas.holmstrom@consector.se>
2020-12-15 11:40:01 +0100

    Final cleanup

--- 15/1.py
+++ 15/1.py
@@ -12,11 +12,10 @@ for t, starting_number in enumerate(data):
 for t in range(len(data), 2020):
     if prev in mem:
         speak = (t - 1) - mem[prev]
-        mem[prev] = t - 1
     else:
         speak = 0
-        mem[prev] = t - 1
 
+    mem[prev] = t - 1
     prev = speak
 
 print(prev)

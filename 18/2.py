3de89a120dddfa8dadd363f69b6d0676e099c6d0
Jonas Holmström <jonas.holmstrom@consector.se>
2020-12-18 12:29:59 +0100

    Day 18

---
+++ 18/2.py
@@ -0,0 +1,20 @@
+import re
+
+def compute(expr):
+    match = re.search(r'\d+ \+ \d+', expr)
+    while match:
+        replacement = '%d' % eval(match.group(0))
+        expr = expr.replace(match.group(0), replacement, 1)
+        match = re.search(r'\d+ \+ \d+', expr)
+    return eval(expr)
+
+def solve(expr):
+    match = re.search(r'\([^\(\)]*\)', expr)
+    while match:
+        replacement = '%d' % compute(match.group(0)[1:-1])
+        expr = expr.replace(match.group(0), replacement)
+        match = re.search(r'\([^\(\)]*\)', expr)
+    return compute(expr)
+
+with open('./data', 'r') as f:
+    print(sum([solve(expr) for expr in f]))

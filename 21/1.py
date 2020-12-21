409b5f61405eb761f31c7e55193d46ff236b3d61
Jonas Holmström <jonas@holmstrom.me>
2020-12-21 18:26:58 +0100

    Fancy &= operator

--- 21/1.py
+++ 21/1.py
@@ -6,13 +6,13 @@ with open('./data') as f:
     for line in f.read().split("\n"):
         ingredients, allergens = line[0:-1].split(' (contains ')
         allergens = allergens.split(', ')
-        ingredients = ingredients.split(' ')
-        all_ingredients = all_ingredients + ingredients
+        ingredients = set(ingredients.split(' '))
+        all_ingredients = all_ingredients + list(ingredients)
         for a in allergens:
             if a not in possible.keys():
-                possible[a] = set(ingredients)
+                possible[a] = ingredients
             else:
-                possible[a] = possible[a].intersection(ingredients)
+                possible[a] &= ingredients
 
 can_have_allergens = set([i for p in possible.values() for i in p])
 

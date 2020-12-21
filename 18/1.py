c087a72c499ddf19ec3344cfef5ce46318aa49cd
Jonas Holmström <jonas.holmstrom@consector.se>
2020-12-21 14:56:12 +0100

    Walrus operator 🔥

--- 18/1.py
+++ 18/1.py
@@ -1,18 +1,12 @@
 import re
 
 def compute(expr):
-    while True:
-        match = re.search(r'\d+ [+*] \d+', expr)
-        if not match:
-            break
+    while match := re.search(r'\d+ [+*] \d+', expr):
         expr = expr[:match.start()] + str(eval(match[0])) + expr[match.end():]
     return eval(expr)
 
 def solve(expr):
-    while True:
-        match = re.search(r'\([^()]+\)', expr)
-        if not match:
-            break
+    while match := re.search(r'\([^()]+\)', expr):
         expr = expr.replace(match[0], str(compute(match[0][1:-1])))
     return compute(expr)
 

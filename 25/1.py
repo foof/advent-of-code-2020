d4bd5caf2c3bcb36b1b2d215bdc76fa0372913d9
Jonas Holmström <jonas.holmstrom@consector.se>
2020-12-25 13:14:10 +0100

    Cleanup

--- 25/1.py
+++ 25/1.py
@@ -3,21 +3,16 @@ with open('./data') as f:
     keys = f.read().split("\n")
     card_public_key = int(keys[0])
     door_public_key = int(keys[1])
+    mod = 20201227
 
 def transform(value, subject_number):
-    value *= subject_number
-    value %= 20201227
-    return value
+    return value * subject_number % mod
 
 value = 1
-door_loop_size = 0
-while value != door_public_key:
-    value = transform(value, 7)
+door_loop_size = 1
+while (value := transform(value, 7)) != door_public_key:
     door_loop_size+=1
-print('door_loop_size', door_loop_size)
 
-encryption_key = 1
-for _ in range(door_loop_size):
-    encryption_key = transform(encryption_key, card_public_key)
+encryption_key = pow(card_public_key, door_loop_size, mod)
 
 print(encryption_key)
